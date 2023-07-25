483dea881e626332265c624f451c9214ba0a9417
dan <dang240@gmail.com>
2023-07-25 18:24:17 +0300

    adding back uri working

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 const io = socketio(server, { cors: corsOptions });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb+srv://dang240:fPrxJ9PBmIm7OzOx@cluster0.2onz8rg.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
