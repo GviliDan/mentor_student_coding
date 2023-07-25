614a230297c7bf6a41d9bf47ec836b64fa9f211b
dan <dang240@gmail.com>
2023-07-25 13:59:04 +0300

    change port2

--- backend/src/server.js
+++ backend/src/server.js
@@ -78,7 +78,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8106;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
