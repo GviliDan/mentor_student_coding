9f441ae8abe8885517dabf2050e17d4a8af4cdc2
dan <dang240@gmail.com>
2023-07-25 23:12:30 +0300

    Updated server URL to new_server_url

--- frontend/src/api/Constants.js
+++ frontend/src/api/Constants.js
@@ -1,4 +1,4 @@
 export const SERVER_URL =
   process.env.NODE_ENV === 'production'
-    ? "https://mentor-student-coding-backend.vercel.app"
+    ? "https://mentor-student-coding.onrender.com/"
     : "http://localhost:8106";
