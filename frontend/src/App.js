75889420ee7deed963107e570c3cd7efaf68c272
dan <dang240@gmail.com>
2023-07-25 13:05:11 +0300

    my commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LobbyPage from './pages/LobbyPage';
-import CodeBlockPage from './pages/components/CodeBlockPage';
+import CodeBlockPage from './pages/CodeBlockPage';
 
 function App() {
   return (
