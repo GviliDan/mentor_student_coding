e992587033a220620be53725cd1a9bd6d8268356
dan <dang240@gmail.com>
2023-07-26 15:06:54 +0300

    fix

--- frontend/src/api/Constants.js
+++ frontend/src/api/Constants.js
@@ -1,3 +1,3 @@
 export const SERVER_URL = process.env.NODE_ENV === 'production'
-    ? "https://mentor-student-coding.onrender.com/"
+    ? "https://mentor-student-front-343ebf81270f.herokuapp.com"
     : "http://localhost:8106";
