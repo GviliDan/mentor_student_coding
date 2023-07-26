4f108142bfb343c5a36a1123cb43a2dcae53f39c
dan <dang240@gmail.com>
2023-07-26 14:57:02 +0300

    fix

--- frontend/src/api/Constants.js
+++ frontend/src/api/Constants.js
@@ -1,6 +1,3 @@
-export const SERVER_URL =
-  process.env.NODE_ENV === 'production'
+export const SERVER_URL = process.env.NODE_ENV === 'production'
     ? "https://mentor-student-coding.onrender.com/"
     : "http://localhost:8106";
-
-export const SERVER_URL = "http://localhost:8106"
--- frontend/src/api/services/CodeBlocksApi.js
+++ frontend/src/api/services/CodeBlocksApi.js
@@ -14,8 +14,3 @@ class CodeBlocksApi {
 }
 
 export default new CodeBlocksApi();
-
-export const SERVER_URL =
-  process.env.NODE_ENV === 'production'
-    ? "https://mentor-student-coding-backend.vercel.app"
-    : "http://localhost:8106";
